fbcf5468596d10365b7ca483578c31366814e393
Charline Menant <mcharline407@hotmail.com>
2024-11-26 15:15:30 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  delete "tasks/:id", to: "tasks#destroy"
+  get "tasks/:id", to: "tasks#show", as: :task
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
